1fd3d30b61aa3cac0ca6f573e517cd57c366aec8
Bridger Tower <bridgertower@gmail.com>
2024-03-04 09:28:10 -0700

    initalize form

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Image from "next/image";
+import { MultiStepForm } from "@/components/form";
 
 export default function Home() {
   return (
     <main className="p-12">
       <p>Hello World</p>
-      <p>
-        This Next JS template has been set up based on the recommended{" "}
-        <a
-          className="underline"
-          href="https://ui.shadcn.com/docs/installation/next"
-        >
-          Shadcn/ui Next.js setup
-        </a>
-        .
-      </p>
+      <MultiStepForm />
     </main>
   );
 }
