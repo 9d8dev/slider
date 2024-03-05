480687cf0c9ad7f3aec5fba88ab4609e70c72a2c
Bridger Tower <bridgertower@gmail.com>
2024-03-05 13:42:41 -0700

    add background image example

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import bg from "@/public/bg.png";
 import { MultiStepForm } from "@/components/multi-step-form";
 
 export default function Home() {
   return (
-    <main className="p-6 md:p-12 h-screen w-screen flex items-center justify-center">
+    <main className="p-6 md:p-12 h-screen w-screen relative flex items-center justify-center">
+      <Image
+        className="absolute -z-10 top-0 right-0 left-0 min-h-screen"
+        src={bg}
+        alt="background"
+      ></Image>
       <MultiStepForm />
     </main>
   );
